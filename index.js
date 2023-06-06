0998b6a67147ebf34e8c3509e6344cbae5dcc689
KasTheDev <118139996+KasTheDev@users.noreply.github.com>
2023-06-05 19:57:49 -0700

    added copy to clipboard function

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const colorSchemeBtn = document.getElementById('color-scheme-btn')
 const colorMode = document.getElementById('color-mode-select')
 const colorContainer = document.getElementById('color-container')
 const hexContainer = document.getElementById('hex-container')
+const copied = document.getElementById('copied')
 const hexArray = []
+let isWaiting = false
 
 colorSchemeBtn.addEventListener('click', fetchColorScheme)
 
@@ -33,8 +35,23 @@ function getColorScheme() {
 function render(){
     const colors = hexArray.map(function(hex){
         colorContainer.innerHTML += `
-        <div style="background-color:#${hex};" class = "color"></div>`
-        hexContainer.innerHTML += `<p>#${hex}</p>`
+        <div data-color=#${hex} style="background-color:#${hex};" class = "color"></div>`
+        hexContainer.innerHTML += `<p data-color=#${hex}>#${hex}</p>`
     })
     
 }
+
+document.addEventListener('click',function(e){
+    if(e.target.dataset.color && isWaiting === false){
+        isWaiting = true
+        navigator.clipboard.writeText(e.target.dataset.color)
+        copied.style.display = "block"
+        copied.style.opacity = 1
+        copied.innerText = `${e.target.dataset.color} copied to clipboard`
+        setTimeout(()=>copied.style.opacity= 0,1000)
+        setTimeout(()=>{
+            copied.style.display = "none"
+            isWaiting = false
+            },1500)
+    }
+})
